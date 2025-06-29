56f408c4559278c6d5582dea0e7ad652592f7141
lioarce01 <lioarce1@gmail.com>
2025-06-29 14:23:05 -0300

    chore: back and front folder name changed

--- backend-nest/prisma/migrations/20250620173936_add_scheduled_datetime_non_nullable/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `scheduled_datetime` on table `adherence` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "public"."adherence" ALTER COLUMN "scheduled_datetime" SET NOT NULL;
